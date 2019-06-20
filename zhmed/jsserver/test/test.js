420306ba888b0e580907b4f712c3eb76769c4dba
qwang@420 <224608422@qq.com>
2019-06-20 10:18:22 +0800

    add test

--- zhmed/jsserver/test/test.js
+++ zhmed/jsserver/test/test.js
@@ -1,6 +1,10 @@
 var assert = require('assert');
-const launch =require("../launch.js")
+const launch =require("../launch.js")("8899")
 const tjson=require("./test.json")
+const path= require("path")
+const pth=path.resolve(".")
+console.log(pth);
+
 const request = require('supertest');
 let url = 'http://127.0.0.1';
 let port="";
@@ -10,7 +14,6 @@ try {
 port=8888
 } catch (error) {
     port=7589
-
 }
 url = url+":"+port;
 console.log(url); 
@@ -20,6 +23,7 @@ const should = require('should');
 describe("Mqtt Test",()=>{
     it("If server started",function(done){
         let result=launch.generateMqttToTup(tjson.mqttGen.getConf.req);
+        done()
         console.log(result);;
         
       
